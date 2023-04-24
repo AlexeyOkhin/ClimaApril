45c242575a953c9958ff695254259f26def4da2b
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-04-24 18:34:04 +0500

    init project

---
+++ ClimaApril/Application/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  ClimaApril
+//
+//  Created by Djinsolobzik on 24.04.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let sceneWindow = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: sceneWindow)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+    }
+}
--- ClimaApril/Modules/MainModule/MainViewController.swift
+++ ClimaApril/Modules/MainModule/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  ClimaApril
 //
 //  Created by Djinsolobzik on 24.04.2023.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
